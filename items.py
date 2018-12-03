c065f4f67be81811bf9adf44198ad98b29d625fc
NoiseGenerator <rfnoisegenerator@gmail.com>
2018-12-03 18:13:23 -0500

    added items and gold and weapons

---
+++ items.py
@@ -0,0 +1,27 @@
+class Item():
+    """The base class for all items"""
+    def __init__(self, name, description, value):
+        self.name = name
+        self.description = description
+        self.value = value
+
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)
+
+class Gold(Item):
+    def __init__(self, amt):
+        self.amt = amt
+        super().__init__(
+                name="Gold",
+                description="A small round coin with {} stamped on the side.".format(str(self.amt)),
+                value=self.amt)
+
+class Weapon(Item):
+    def __init__(self, name, description, value, damage):
+        self.damage = damage
+        super().init(name, description, value)
+
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name, self.description, self.value, self.damage)
+
+
